0e259087875993e6fdbaefc1400ddf48294bf942
Jiang Jing <jasperjiang97@gmail.com>
2018-05-06 20:02:30 -0400

    bbb

--- truffle-source/test/TestDigiArtPlat.sol
+++ truffle-source/test/TestDigiArtPlat.sol
@@ -6,12 +6,5 @@ import "../contracts/DigiArtPlat.sol";
 
 contract TestDigiArtPlat {
     DigiArtPlat plat = DigiArtPlat(DeployedAddresses.DigiArtPlat());
-
-    // function testUserCanBuy() public {
-    //     uint returnedId = plat.buy(8);
-
-    //     uint expected = 8;
-
-    //     Assert.equal(returnedId, expected, "Buy 8.");
-    // }   
+ 
 }
